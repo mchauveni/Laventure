f59ea3dfc06fccb2d1db75cb41d5ae6511bac4e2
mchauveni <matheo.chaumet.venier@gmail.com>
2023-03-21 09:47:05 +0100

    placeComponent init

---
+++ src/lib/PlaceComponent.svelte
@@ -0,0 +1,17 @@
+
+<div class="placeComponent">
+  <picture class="placeComponent__background">
+    <source type="image/webp" srcset="https://placeimg.com/640/480/nature" />
+    <source type="image/jpeg" srcset="https://placeimg.com/640/480/nature" />
+    <img src="https://placeimg.com/640/480/nature" alt="background_image"/>
+  </picture>
+
+  <h3 class="placeComponent__title">Lorem Ipsum</h3>
+  <div class="placeComponent__descriptionWrapper">
+    <picture class="placeComponent__locationIcon">
+      <source media="" srcset="/src/assets/img/location_icon.svg" />
+      <img alt="location_icon" />
+    </picture>
+    <p class="placeComponent__desc">Lorem</p>
+  </div>
+</div>

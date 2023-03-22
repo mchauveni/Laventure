2286e9e0d7b2fbaa5b17a08d1b3690f3696e6267
mchauveni <matheo.chaumet.venier@gmail.com>
2023-03-22 14:29:29 +0100

    placeComponent works and is modulable kinda

--- src/App.svelte
+++ src/App.svelte
@@ -2,4 +2,14 @@
   import PlaceComponent from "./lib/PlaceComponent.svelte";
 </script>
 
-<PlaceComponent />
+<PlaceComponent
+  placeTitle="Abbaye de l'embarcadère"
+  placeLocation="Maillezais"
+  backgroundImageURL="https://placeimg.com/640/480/nature"
+/>
+
+<PlaceComponent
+  placeTitle="Autre endroit"
+  placeLocation="Angoulême"
+  backgroundImageURL="https://placeimg.com/640/480/nature"
+/>

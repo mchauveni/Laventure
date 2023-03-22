84e10f7a171375329eb2835aee06f0c6ebbc1709
mchauveni <matheo.chaumet.venier@gmail.com>
2023-03-22 18:05:27 +0100

    pages are now svelte component

--- src/App.svelte
+++ src/App.svelte
@@ -1,43 +1,69 @@
 <script>
   import Footer from "./lib/Footer.svelte";
+  import Home from "./lib/Home.svelte";
+  import Like from "./lib/Like.svelte";
+  import List from "./lib/List.svelte";
+  import Map from "./lib/Map.svelte";
   import PlaceComponent from "./lib/PlaceComponent.svelte";
+
+  let pageState = "map";
 </script>
 
-<header class="header">
-  <section class="header__design">
-    <div class="header__design--first" />
-    <div class="header__design--second" />
-    <div class="header__design--third" />
-    <div class="header__design--fourth" />
-  </section>
-  <section class="header__logo">
-    <img
-      class="header__logo--img"
-      srcset="./src/assets/img/Logo_type.png 400w"
-      alt="Logo de notre application"
-    />
-  </section>
-</header>
-<main class="main">
-  <section class="main__title">
-    <h1>
-      La France est belle, <br />
-      <span class="main__title--span"> Découvrez la, avec son histoire !</span>
-    </h1>
-  </section>
-  <section class="main__illustration">
-    <img
-      class="main__illustration--img"
-      srcset="
+<div class="page" class:visible={pageState === "index"}>
+  <header class="header">
+    <section class="header__design">
+      <div class="header__design--first" />
+      <div class="header__design--second" />
+      <div class="header__design--third" />
+      <div class="header__design--fourth" />
+    </section>
+    <section class="header__logo">
+      <img
+        class="header__logo--img"
+        srcset="./src/assets/img/Logo_type.png 400w"
+        alt="Logo de notre application"
+      />
+    </section>
+  </header>
+  <main class="main">
+    <section class="main__title">
+      <h1>
+        La France est belle, <br />
+        <span class="main__title--span">
+          Découvrez la, avec son histoire !</span
+        >
+      </h1>
+    </section>
+    <section class="main__illustration">
+      <img
+        class="main__illustration--img"
+        srcset="
           ./src/assets/img/original-c48f63577ee2eb3dd211ce7065d80b72-removebg-preview.png 400w
         "
-      alt="Illustration du voyage"
-    />
-  </section>
-  <section class="main__btn">
-    <a href="/src/pages/home.html" class="main__btn--btn">
-      Partir à l'aventure
-    </a>
-    <a href="#" class="main__btn--mtn_legal"> Mentions légales</a>
-  </section>
-</main>
+        alt="Illustration du voyage"
+      />
+    </section>
+    <section class="main__btn">
+      <a href="/src/pages/home.html" class="main__btn--btn">
+        Partir à l'aventure
+      </a>
+      <a href="." class="main__btn--mtn_legal"> Mentions légales</a>
+    </section>
+  </main>
+</div>
+
+<div class="page" class:visible={pageState === "home"}>
+  <Home />
+</div>
+
+<div class="page" class:visible={pageState === "map"}>
+  <Map />
+</div>
+
+<div class="page" class:visible={pageState === "list"}>
+  <List />
+</div>
+
+<div class="page" class:visible={pageState === "like"}>
+  <Like />
+</div>

ca4f63ba7dc8ee151240d2ff0228d001e7232dbf
Kaosbeing <matheorigami@gmail.com>
2023-03-29 12:52:20 +0200

    loader + map tweaks

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,3 +1,7 @@
 import { writable } from "svelte/store";
 
+/* must be ["index", "home", "map", "list", "like"] */
 export const pageState = writable("map");
+
+/* must be ["none", "loader", "focus"] */
+export const overlay = writable("loader");

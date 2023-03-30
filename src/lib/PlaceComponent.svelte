b2de01dfb862215875ed09367985c925f7c56141
mchauveni <matheo.chaumet.venier@gmail.com>
2023-03-30 12:05:48 +0200

    oopsie

--- src/lib/PlaceComponent.svelte
+++ src/lib/PlaceComponent.svelte
@@ -3,8 +3,6 @@
   import { overlay, focusData } from "./stores";
   let allplaces = data.places;
 
-  export let showDeleteBtn = false;
-
   function placeClick(place) {
     overlay.set("focus");
     focusData.set(place);
@@ -20,9 +18,6 @@
     }}
   >
     <div class="placeComponent__descWrapper">
-      {#if { showDeleteBtn }}
-        <img src="" alt="" />
-      {/if}
       <h3 class="placeComponent__title">{place.name}</h3>
       <div class="placeComponent__locationWrapper">
         <img

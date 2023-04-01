0d48f769296d8d918332f4f607c8e717a0cf58ed
Kaosbeing <matheorigami@gmail.com>
2023-04-01 18:08:21 +0200

    Searchbar now filters correctly the places

--- src/lib/PlaceComponent.svelte
+++ src/lib/PlaceComponent.svelte
@@ -1,32 +1,32 @@
 <script>
-  import data from "./../scripts/place.json";
-  import { overlay, focusData } from "./stores";
-  let allplaces = data.places;
+    import data from "/src/scripts/place.json";
+    import { overlay, focusData, search } from "./stores";
+    import { fade } from "svelte/transition";
+    let allplaces = data.places;
 
-  function placeClick(place) {
-    overlay.set("focus");
-    focusData.set(place);
-  }
+    function placeClick(place) {
+        overlay.set("focus");
+        focusData.set(place);
+    }
 </script>
 
 {#each allplaces as place}
-  <button
-    class="placeComponent placeComponent__background"
-    style="background-image: url({place.imgSrc});"
-    on:click|preventDefault={() => {
-      placeClick(place);
-    }}
-  >
-    <div class="placeComponent__descWrapper">
-      <h3 class="placeComponent__title">{place.name}</h3>
-      <div class="placeComponent__locationWrapper">
-        <img
-          class="placeComponent__locationIcon"
-          src="/src/assets/img/icons/location_icon.svg"
-          alt="location_icon"
-        />
-        <p class="placeComponent__location">{place.namePlace}</p>
-      </div>
-    </div>
-  </button>
+    {#if place.name.toUpperCase().includes($search.toUpperCase()) || place.namePlace.toUpperCase().includes($search.toUpperCase())}
+        <button
+            class="placeComponent placeComponent__background"
+            style="background-image: url({place.imgSrc});"
+            on:click|preventDefault={() => {
+                placeClick(place);
+            }}
+            transition:fade={{ duration: 200 }}
+        >
+            <div class="placeComponent__descWrapper">
+                <h3 class="placeComponent__title">{place.name}</h3>
+                <div class="placeComponent__locationWrapper">
+                    <img class="placeComponent__locationIcon" src="/src/assets/img/icons/location_icon.svg" alt="location_icon" />
+                    <p class="placeComponent__location">{place.namePlace}</p>
+                </div>
+            </div>
+        </button>
+    {/if}
 {/each}
